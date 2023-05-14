39d8311cd545d8ef74058a7a6ff946776c35cfd1
nakasyou <syoutarou1103@outlook.jp>
2023-05-14 02:03:26 +0000

    Route

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,16 +3,18 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[name].tsx";
+import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/index.tsx";
+import * as $3 from "./routes/notes/[id].tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
+    "./routes/_404.tsx": $0,
     "./routes/api/joke.ts": $1,
     "./routes/index.tsx": $2,
+    "./routes/notes/[id].tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
