ae21f90064f4e38fd51a720cb0ddfb204b37166b
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-14 10:48:04 +0900

    fix style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 export default () => {
-  return <div class="w-full flex drop-shadow-md">
+  return <div class="w-full flex drop-shadow-xl top-0 sticky">
     <div>
       <a href="/">
         <img src="/icon.svg" alt="NanohaNote icon" />
