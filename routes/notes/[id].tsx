7b9946f0301412d83704e1ed4e98d626f5f3a999
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-14 11:54:12 +0900

    Update [id].tsx

--- routes/notes/[id].tsx
+++ routes/notes/[id].tsx
@@ -1,14 +1,17 @@
 import { Head } from "$fresh/runtime.ts"
 import { PageProps } from "$fresh/server.ts"
+import { h, useState } from "$fresh/runtime.ts";
 
 export default (props: PageProps) => {
-  
+  const [text,setText] = useState(`
+    aaaaaaaa
+  `)
   return <>
     <Head>
       <title>{ props.params.id } | NanohaNote</title>
     </Head>
     <div>
-      
+      {text}
     </div>
   </>
 }
