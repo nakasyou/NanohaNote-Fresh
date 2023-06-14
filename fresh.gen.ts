50f49f3551b2477c86c1bd16755e4943cf6c571d
nakasyou <syoutarou1103@outlook.jp>
2023-06-14 08:22:46 +0000

    update manifest

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -8,6 +8,7 @@ import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/index.tsx";
 import * as $3 from "./routes/notes/[id].tsx";
 import * as $$0 from "./islands/Counter.tsx";
+import * as $$1 from "./islands/Note.tsx";
 
 const manifest = {
   routes: {
@@ -18,6 +19,7 @@ const manifest = {
   },
   islands: {
     "./islands/Counter.tsx": $$0,
+    "./islands/Note.tsx": $$1,
   },
   baseUrl: import.meta.url,
   config,
