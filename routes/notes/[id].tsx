424690ddea748bc71150b23fed005716616a7a4f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-14 17:19:35 +0900

    Update [id].tsx

--- routes/notes/[id].tsx
+++ routes/notes/[id].tsx
@@ -2,18 +2,15 @@ import { Head } from "$fresh/runtime.ts"
 import { PageProps } from "$fresh/server.ts"
 import { useState } from "preact/hooks"
 import Sheet from "../../components/notes/sheet.tsx"
+import Note from "../../islands/Note.tsx"
 
 export default (props: PageProps) => {
-  const [text,setText] = useState(``)
-  const onInput = e => {
-    setText(e.target.innerHTML)
-  }
   return <>
     <Head>
       <title>{ props.params.id } | NanohaNote</title>
     </Head>
     <div>
-      <textarea></textarea>
+      <Note pageProps={props} />
     </div>
   </>
 }
