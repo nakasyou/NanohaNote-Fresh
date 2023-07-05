e46b43f43ef3e6998681781adf814ad4b391a608
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-05 21:06:38 +0900

    Editor

--- islands/Note.tsx
+++ islands/Note.tsx
@@ -1,10 +1,26 @@
 import { PageProps } from "$fresh/server.ts"
+import { useRef } from "preact/hooks"
 
 export interface Props {
   pageProps: PageProps,
 }
 export default function(props: Props){
+  import {
+    Editor,
+  } from '@tiptap/core'
+  import StarterKit from '@tiptap/starter-kit'
+
+  const noteRef = useRef(null)
+  const editor = new Editor({
+    element: noteRef.current,
+    extensions: [
+      StarterKit,
+    ],
+    content: [...Array(10)].map(()=>'<p>これはTipTapのテストですわよ。<span class="nanoha-sheet">a</span></p>').join(""),
+  })
+  
   return <>
     <div>NanohaNote</div>
+    <div ref={noteRef}>a</div>
   </>
 }
