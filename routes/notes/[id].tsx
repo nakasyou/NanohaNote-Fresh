8ad90a53869ee5cb019c43d85a993b52317239a6
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-15 15:06:48 +0900

    fix

--- routes/notes/[id].tsx
+++ routes/notes/[id].tsx
@@ -1,7 +1,5 @@
 import { Head } from "$fresh/runtime.ts"
 import { PageProps } from "$fresh/server.ts"
-import { useState } from "preact/hooks"
-import Sheet from "../../components/notes/sheet.tsx"
 import Note from "../../islands/Note.tsx"
 
 export default (props: PageProps) => {
