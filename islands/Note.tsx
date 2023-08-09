0876ba8ab4acf274616459bf50b6a0db3a85ca2f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-09 11:37:05 +0900

    Update Note.tsx

--- islands/Note.tsx
+++ islands/Note.tsx
@@ -1,15 +1,19 @@
 import { PageProps } from "$fresh/server.ts"
 import { useEditor, EditorContent } from 'tiptap-preact'
 import StarterKit from '@tiptap/starter-kit'
+import { useEffect, useState } from "preact/hooks"
 
 const Tiptap = () => {
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-    ],
-    content: '<p>Hello World!</p>',
-  })
-
+  const [editor, setEditor] = useState(null)
+  useEffect(() => {
+    const editor = useEditor({
+      extensions: [
+        StarterKit,
+      ],
+      content: '<p>Hello World!</p>',
+    })
+    setEditor(editor)
+  }, [])
   return (
     <EditorContent editor={editor} />
   )
